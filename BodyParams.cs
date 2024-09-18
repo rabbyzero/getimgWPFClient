88f9b8e1af3dacd67bf8574ee2d8193b4eac9d14
rabbyzero@gmail.com <rabbyzero@gmail.com>
2024-09-18 22:51:53 +0800

    09/18 commit 2.

--- BodyParams.cs
+++ BodyParams.cs
@@ -1,6 +1,27 @@
-﻿namespace getimgWPFClient
+﻿using System.Text.Json.Serialization;
+
+namespace getimgWPFClient
 {
-    internal abstract class BodyParams
+    internal class BodyParams<MODEL,MODELMETHOD> 
+        where MODEL:ImageModel 
+        where MODELMETHOD : ModelMethod
     {
+        //String model;
+        //String negative_prompt;
+        //String prompt2;
+        //String negative_prompt2;
+        //String guidance;
+        //String scheduler;
+
+        [JsonPropertyName("prompt")] public String Prompt { set; get; } = "";
+        [JsonPropertyName("width")] public int Width { get; set; } = 1024;
+        [JsonPropertyName("height")] public int Height { get; set; } = 1024;
+        [JsonPropertyName("steps")] public int Steps { get; set; } = 1;
+        [JsonPropertyName("seed")] public int Seed { get; set; } = 1;
+        [JsonPropertyName("output_format")] public String Output_format { get; set; } = "jpeg";
+        [JsonPropertyName("respone_format")] public String Response_format { get; set; } = "b64";
+
+        BodyParams() { }
+        
     }
 }
--- TextToImage.cs
+++ TextToImage.cs
@@ -3,16 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace getimgWPFClient
 {
-    internal class TextToImage: ModelMethod
+    internal class TextToImage<MODEL> : ModelMethod where MODEL : ImageModel
     {
         TextToImage()
         {
             methodLocation = "text-to-image";
             httpMethod = Method.Post;
         }
+
+        TextToImage(FLUXSchnell imageModel):this()
+        {
+        }
+
     }
 }
